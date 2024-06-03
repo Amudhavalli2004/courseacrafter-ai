db3e94321792c4198b57d817c0e5b15b6885447b
Amudhavalli2004 <amudhameenaravi@gmail..com>
2024-06-03 06:34:08 +0530

    initial commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,32 @@
+import type { Metadata } from "next";
+import { Lexend } from "next/font/google";
+import "./globals.css";
+import { cn } from "@/lib/utils";
+import Navbar from "@/components/Navbar";
+import { Provider } from "@/components/Providers";
+import { Toaster } from "@/components/ui/toaster";
+
+const lexend = Lexend({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "AI Course Generator",
+ 
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={cn(lexend.className, "antialiased min-h-screen pt-16")}>
+        <Provider>
+          <Navbar />
+          {children}
+          <Toaster/>
+        </Provider>
+      </body>
+    </html>
+  );
+}
